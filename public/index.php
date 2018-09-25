80255e8c906e4878291b6acb722591d32a7afcf2
Laura Lenik <Laura.Lenik78@gmail.com>
2018-09-25 15:23:59 +0200

    mise en ligne

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../vendor/autoload.php';
+
+
+// Créer un nouvel objet de la classe Hello
+
+$objet = new App\Wcs\Hello();
+
+
+// Retourner Hello World
+
+echo $objet->talk();
+
+
+
+  ?>
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Wcs {
+
+
+
+class Hello {     // Création de la class Hello
+
+public function talk(){    // Création d'une méthode talk
+
+    return 'Hello World !';     // La fonction talk retourne 'Hello World !'
+
+    }
+                      }
+
+}
+
+ ?>
